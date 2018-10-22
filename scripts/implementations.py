1c44283bab80a7d5818651edd20bddca9b551c41
Costanza Volpini <costanza.volpini@gmail.com>
2018-10-22 10:23:51 +0200

    Added cross validation and other stuff

---
+++ scripts/implementations.py
@@ -0,0 +1,97 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+
+# TODO: Return type: Note that all functions should return: (w, loss), which is the last weight vector of the
+# method, and the corresponding loss value (cost function). Note that while in previous labs you might have
+# kept track of all encountered w for iterative methods, here we only want the last one.
+
+# Helper functions
+def calculate_mse(e, y):
+    """This function returns the mean-squared error given the error"""
+    return 1/(len(y)) * np.sum(e**2)
+
+def calculate_mae(e):
+    """Calculate the mae for vector e."""
+    return np.mean(np.abs(e))
+
+
+def compute_loss(y, tx, w):
+    """Calculate the loss using mse or mae.
+    """
+    e = y - tx.dot(w)
+    return calculate_mse(e, y)
+
+
+# Linear regression using gradient descent (Yousskit)
+def gradient_descent(y, tx, initial_w, max_iters, gamma):
+    """Gradient descent algorithm."""
+    # Define parameters to store w and loss
+    ws = [initial_w]
+    losses = []
+    w = initial_w
+    for n_iter in range(max_iters):
+        # compute loss, gradient
+        grad, err = compute_gradient(y, tx, w)
+        loss = calculate_mse(err, y)
+        # gradient w by descent update
+        w = w - gamma * grad
+        # store w and loss
+        ws.append(w)
+        losses.append(loss)
+        print("Gradient Descent({bi}/{ti}): loss={l}, w0={w0}, w1={w1}".format(
+              bi=n_iter, ti=max_iters - 1, l=loss, w0=w[0], w1=w[1]))
+
+    return losses, ws
+
+# Linear regression using stochastic gradient descent (Abranches)
+def compute_stoch_gradient(y, tx, w):
+    """Compute a stochastic gradient from just few examples n and their corresponding y_n labels."""
+    err = y - tx.dot(w)
+    grad = -tx.T.dot(err) / len(err)
+    return grad, err
+
+def stochastic_gradient_descent(y, tx, initial_w, batch_size, max_iters, gamma):
+    """Stochastic gradient descent."""
+    # Define parameters to store w and loss
+    ws = [initial_w]
+    losses = []
+    w = initial_w
+
+    for n_iter in range(max_iters):
+        for y_batch, tx_batch in batch_iter(y, tx, batch_size=batch_size, num_batches=1):
+            # compute a stochastic gradient and loss
+            grad, _ = compute_stoch_gradient(y_batch, tx_batch, w)
+            # update w through the stochastic gradient update
+            w = w - gamma * grad
+            # calculate loss
+            loss = compute_loss(y, tx, w)
+            # store w and loss
+            ws.append(w)
+            losses.append(loss)
+
+        print("SGD({bi}/{ti}): loss={l}, w0={w0}, w1={w1}".format(
+              bi=n_iter, ti=max_iters - 1, l=loss, w0=w[0], w1=w[1]))
+    return losses, ws
+
+# Least squares regression using normal equations (Costanza)
+def least_squares(y, tx):
+    """This function return the optimal weights (using QR), and the mean-squared error"""
+    X = tx.T.dot(tx)
+    b = tx.T.dot(y)
+    w_optimal = np.linalg.solve(X, b)
+    e = np.subtract(y, np.dot(tx, w_optimal))
+    return calculate_mse(e, y), w_optimal
+
+# Ridge regression using normal equations (Costanza)
+def ridge_regression(y, tx, lambda_):
+    """implement ridge regression."""
+    aI = 2 * tx.shape[0] * lambda_ * np.identity(tx.shape[1])
+    a = tx.T.dot(tx) + aI
+    b = tx.T.dot(y)
+    return np.linalg.solve(a, b)
+
+# Logistic regression using gradient descent or SGD (Abranches)
+#TODO
+
+# Regularized logistic regression using gradient descent or SGD (Yousskit)
+#TODO
