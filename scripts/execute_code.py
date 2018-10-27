233b76123a6d68b011827641b8a63bec063ee62e
Costanza Volpini <costanza.volpini@gmail.com>
2018-10-27 14:12:06 +0200

    code to execute all methods

--- scripts/execute_code.py
+++ scripts/execute_code.py
@@ -1,3 +1,57 @@
+import numpy as np
+from implementations import *
+from proj1_helpers import *
+from cross_validation import *
+from pre_processing import *
+from split_jet_num import generate_4_sets_looking_on_jetnum, columns_contains_just_missing_values, columns_contains_same_value
+
+
+def divide_dataset_looking_jetnum_and_remove_features(y, tx, ids, tx_test, ids_test):
+    """ Divide the dataset looking on jet_num feature (column 22 of tx).
+        Input:
+            y: labels
+            tx: features
+            ids: event ids
+            tx_test: test features
+            ids_test: test event ids
+        Output:
+            tx_dropped_columns: features without columns containing just constant values
+            features_dropped_0: features without columns containing just constant values where jet_num is equal to 0
+            features_dropped_1: features without columns containing just constant values where jet_num is equal to 1
+            features_dropped_2: features without columns containing just constant values where jet_num is equal to 2
+            features_dropped_3: features without columns containing just constant values where jet_num is equal to 3
+            y_jet_0: labels with shape compatibles with features_dropped_0
+            y_jet_1: labels with shape compatibles with features_dropped_1
+            y_jet_2: labels with shape compatibles with features_dropped_2
+            y_jet_3: labels with shape compatibles with features_dropped_3
+            ids_jet_0: test event ids with shape compatibles with features_dropped_0
+            ids_jet_1: test event ids with shape compatibles with features_dropped_1
+            ids_jet_2: test event ids with shape compatibles with features_dropped_2
+            ids_jet_3: test event ids with shape compatibles with features_dropped_3
+    """
+    # If PRI_jet_num is zero or one then some features are -999. Divide dataset in 4 looking on jet_num 0, 1, 2 and 3.
+    features_jet_0, features_jet_1, features_jet_2, features_jet_3, y_jet_0, y_jet_1, y_jet_2, y_jet_3, ids_jet_0, ids_jet_1, ids_jet_2, ids_jet_3 = generate_4_sets_looking_on_jetnum(tx, y, ids)
+
+    # For each set look how many missing values there are.. in order to detect how many features we want to drop!
+    # iterate to find which columns to drop --> Check for constant values, if I feature contains all the same values it is not important.
+    columns_to_remove_0 = columns_contains_same_value(features_jet_0[0])
+    columns_to_remove_1 = columns_contains_same_value(features_jet_1[0])
+    columns_to_remove_2 = columns_contains_same_value(features_jet_2[0])
+    columns_to_remove_3 = columns_contains_same_value(features_jet_3[0])
+
+    # remove columns from subset
+    features_dropped_0 = np.delete(features_jet_0[0], columns_to_remove_0, axis=1)
+    features_dropped_1 = np.delete(features_jet_1[0], columns_to_remove_1, axis=1)
+    features_dropped_2 = np.delete(features_jet_2[0], columns_to_remove_2, axis=1)
+    features_dropped_3 = np.delete(features_jet_3[0], columns_to_remove_3, axis=1)
+
+    # Columns missing in all subsets
+    columns_to_remove = [4, 5, 6, 12, 22, 26, 27, 28]
+
+    tx_dropped_columns = np.delete(tx, columns_to_remove, axis=1)
+    return tx_dropped_columns, features_dropped_0, features_dropped_1, features_dropped_2, features_dropped_3, y_jet_0, y_jet_1, y_jet_2, y_jet_3, ids_jet_0, ids_jet_1, ids_jet_2, ids_jet_3
+
+
 # Execute a method with or without cross_validation
 def execute_one_method(y, tx, ids, tx_test, ids_test, method_name, cross_validation_flag, m, **args):
     if(cross_validation_flag):
@@ -32,7 +86,7 @@ def execute_one_method(y, tx, ids, tx_test, ids_test, method_name, cross_validat
         max_accuracy_train = np.max(accuracy_train)
 
         print(method_name)
-        print("\n Accuracy test, mean: %f, min value: %f, max value: %f \n" %(mean_accuracy_test, min_accuracy_test, max_accuracy_test))
+        print("\nAccuracy test, mean: %f, min value: %f, max value: %f \n" %(mean_accuracy_test, min_accuracy_test, max_accuracy_test))
         print("Accuracy train, mean: %f, min value: %f, max value: %f \n" %(mean_accuracy_train, min_accuracy_train, max_accuracy_train))
     else:
         loss, w = m(y, tx, **args)
@@ -43,7 +97,7 @@ def execute_one_method(y, tx, ids, tx_test, ids_test, method_name, cross_validat
         # calculate the accuracy for train and test data
         accuracy_train = calculate_accuracy(y_predicted, y)
         print(method_name)
-        print("\n Accuracy train value: %f \n" %(accuracy_train))
+        print("\nAccuracy train value: %f \n" %(accuracy_train))
 
 # Return a new dataset where missing values are replaced by mean
 def replace_set_mean(tx):
@@ -75,36 +129,13 @@ def execute_one_method_median(y, tx, ids, tx_test, ids_test, method_name, cross_
 def execute_one_method_normalized(y, tx, ids, tx_test, ids_test, method_name, cross_validation_flag, m, **args):
     execute_one_method(y, replace_set_normalize(tx), ids, replace_set_normalize(tx_test), ids_test, method_name, cross_validation_flag, m, **args)
 
-def divide_dataset_looking_jetnum_and_remove_features(y, tx, ids, tx_test, ids_test):
-    # If PRI_jet_num is zero or one then some features are -999. Divide dataset in 4 looking on jet_num 0, 1, 2 and 3.
-    features_jet_0, features_jet_1, features_jet_2, features_jet_3, y_jet_0, y_jet_1, y_jet_2, y_jet_3, ids_jet_0, ids_jet_1, ids_jet_2, ids_jet_3 = generate_4_sets_looking_on_jetnum(tx, y, ids)
-
-    # For each set look how many missing values there are.. in order to detect how many features we want to drop!
-    # iterate to find which columns to drop --> Check for constant values, if I feature contains all the same values it is not important.
-    columns_to_remove_0_b = columns_contains_same_value(features_jet_0[0])
-    columns_to_remove_1_b = columns_contains_same_value(features_jet_1[0])
-    columns_to_remove_2_b = columns_contains_same_value(features_jet_2[0])
-    columns_to_remove_3_b = columns_contains_same_value(features_jet_3[0])
-
-    # remove columns from subset
-    features_dropped_0 = np.delete(features_jet_0[0], columns_to_remove_0_b, axis=1)
-    features_dropped_1 = np.delete(features_jet_1[0], columns_to_remove_1_b, axis=1)
-    features_dropped_2 = np.delete(features_jet_2[0], columns_to_remove_2_b, axis=1)
-    features_dropped_3 = np.delete(features_jet_3[0], columns_to_remove_3_b, axis=1)
-
-    # Columns missing in all subsets
-    columns_to_remove = [4, 5, 6, 12, 22, 26, 27, 28]
-
-    tx_dropped_columns = np.delete(tx, columns_to_remove, axis=1)
-    return tx_dropped_columns, features_dropped_0, features_dropped_1, features_dropped_2, features_dropped_3
-
 def execute_all_methods(y, tx, ids, tx_test, ids_test, cross_validation_flag, **args):
-    execute_one_method(y, tx, ids, tx_test, ids_test, "least square", cross_validation_flag, least_squares)
-    execute_one_method(y, tx, ids, tx_test, ids_test, "ridge regression", cross_validation_flag, ridge_regression, lambda_)
+    execute_one_method(y, tx, ids, tx_test, ids_test, "1. LEAST SQUARE", cross_validation_flag, least_squares)
+    execute_one_method(y, tx, ids, tx_test, ids_test, "2. RIDGE REGRESSION", cross_validation_flag, ridge_regression, **args)
     # ADD OTHER METHODS!!!!!!!!!
 
 def execute_all_methods_median(y, tx, ids, tx_test, ids_test, cross_validation_flag, **args):
-    # Find mean and replace missing values with mean
+    # Find median and replace missing values with median
     tx_m = replace_set_median(tx)
     tx_test_m = replace_set_median(tx_test)
     execute_all_methods(y, tx_m, ids, tx_test_m, ids_test, cross_validation_flag, **args)
@@ -116,6 +147,7 @@ def execute_all_methods_mean(y, tx, ids, tx_test, ids_test, cross_validation_fla
     execute_all_methods(y, tx_m, ids, tx_test_m, ids_test, cross_validation_flag, **args)
 
 def execute_all_methods_normalize(y, tx, ids, tx_test, ids_test, cross_validation_flag, **args):
+    # Normalize replacing missing values with zero
     tx_m = replace_set_normalize(tx)
     tx_test_m = replace_set_normalize(tx_test)
     execute_all_methods(y, tx_m, ids, tx_test_m, ids_test, cross_validation_flag, **args)
--- scripts/implementations.py
+++ scripts/implementations.py
@@ -66,7 +66,7 @@ def least_squares(y, tx):
 
 # Ridge regression using normal equations
 def ridge_regression(y, tx, lambda_):
-    """implement ridge regression."""
+    """ridge regression."""
     aI = 2 * tx.shape[0] * lambda_ * np.identity(tx.shape[1])
     a = tx.T.dot(tx) + aI
     b = tx.T.dot(y)
