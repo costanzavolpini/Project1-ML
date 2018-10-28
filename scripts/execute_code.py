Fixed least_squares_GD (added normalization)

scripts/execute_code.py
@@ -3,7 +3,7 @@ from implementations import *
 from proj1_helpers import *
 from cross_validation import *
 from pre_processing import *
-from split_jet_num import generate_4_sets_looking_on_jetnum, columns_contains_just_missing_values, columns_contains_same_value, reasseambly_tx_labels_ids
+from split_jet_num import generate_4_sets_looking_on_jetnum, columns_contains_just_missing_values, columns_contains_same_value
 
 
 def divide_dataset_looking_jetnum_and_remove_features(y, tx, ids):
@@ -128,11 +128,15 @@ def execute_all_methods(y, tx, ids, cross_validation_flag, **args):
     max_accuracy = accuracy1
     method_name_selected = method_name1
     w_final = w1
-    accuracy2, method_name2, w2 = execute_one_method(y, tx, ids, "2. RIDGE REGRESSION", cross_validation_flag, ridge_regression, **args)
+
+    accuracy2, method_name2, w2 = execute_one_method(y, tx, ids, "2. RIDGE REGRESSION", cross_validation_flag, ridge_regression, lambda_=args["lambda_"])
     if(accuracy2 > accuracy1):
         max_accuracy = accuracy2
         method_name_selected = method_name2
         w_final = w2
+
+    accuracy3, method_name3, w3 = execute_one_method(y, tx, ids, "3. GRADIENT DESCENT", cross_validation_flag, least_squares_GD, initial_w=args["initial_w"], max_iters=args["max_iters"], gamma=args["gamma"])
+
     return max_accuracy, method_name_selected, w_final
     # ADD OTHER METHODS!!!!!!!!!
 
@@ -158,8 +162,6 @@ def execute_all_methods_normalize(y, tx, ids, cross_validation_flag, **args):
     return acc, method_selec, w
 
 def generate_submission(tx_old, tx0, tx1, tx2, tx3, ids0, ids1, ids2, ids3, w0, w1, w2, w3, name, degree):
-    # reasseambly_tx_labels_ids(tx_old, tx0, tx1, tx2, tx3, ids0, ids1, ids2, ids3, w0, w1, w2, w3)
-    # tx, ids, w = reasseambly_tx_labels_ids(tx_old, tx0, tx1, tx2, tx3, ids0, ids1, ids2, ids3, w0, w1, w2, w3)
     y_test_predicted0 = []
     test_poly0 = build_poly(tx0, degree)
     y_test_predicted0 = predict_labels(w0, test_poly0)
@@ -179,4 +181,3 @@ def generate_submission(tx_old, tx0, tx1, tx2, tx3, ids0, ids1, ids2, ids3, w0, 
     id_final = np.concatenate((ids0, ids1, ids2, ids3), axis=0)
     y_pred_final = np.concatenate((y_test_predicted0, y_test_predicted1, y_test_predicted2, y_test_predicted3), axis=0)
     create_csv_submission(id_final, y_pred_final, name)
-    # return True

scripts/implementations.py
@@ -7,19 +7,21 @@ from helpers import *
 # kept track of all encountered w for iterative methods, here we only want the last one.
 
 # Linear regression using gradient descent
-def gradient_descent(y, tx, initial_w, max_iters, gamma):
+def least_squares_GD(y, tx, initial_w, max_iters, gamma):
     """Gradient descent algorithm."""
     # Define parameters to store w and loss
-    if(initial_w.shape[0] != tx.shape[1]):
-          initial_w = np.zeros(tx.shape[1])
+    if(initial_w == None):
+          initial_w = initialize_weight(tx.shape[1])
     loss = 0
     w = initial_w
+    print(tx.shape, y.shape)
     for n_iter in range(max_iters):
         # compute loss, gradient
         grad, err = compute_gradient(y, tx, w)
-        loss = calculate_mse(err, y)
         # gradient w by descent update
         w = w - gamma * grad
+        print("gradient", gamma, grad)
+    loss = calculate_mse(err, y)
     return loss, w
 
 # Linear regression using stochastic gradient descent
@@ -32,6 +34,8 @@ def compute_stoch_gradient(y, tx, w):
 def stochastic_gradient_descent(y, tx, initial_w, batch_size, max_iters, gamma):
     """Stochastic gradient descent."""
     # Define parameters to store w and loss
+    if(initial_w == None):
+        initial_w = initialize_weight(tx.shape[1])
     ws = [initial_w]
     losses = []
     w = initial_w
@@ -65,7 +69,7 @@ def least_squares(y, tx):
     return loss, w
 
 # Ridge regression using normal equations
-def ridge_regression(y, tx, lambda_):
+def ridge_regression(y, tx, lambda_, **args):
     """ridge regression."""
     aI = 2 * tx.shape[0] * lambda_ * np.identity(tx.shape[1])
     a = tx.T.dot(tx) + aI